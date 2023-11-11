a7c5368dc250d494188fe503de09c14b2e4f2a58
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2023-11-11 14:32:25 +0800

    Update C++字符串的一些操作.cpp

--- C++字符串的一些操作.cpp
+++ C++字符串的一些操作.cpp
@@ -21,7 +21,10 @@ F10 run
     cout<< q<<'\n'<<r<<'\n'<<t;
     bool g=isupper('A'); //判斷字元是否為大寫，用於字符串需要搭配for循環
     cout<<"he22llo \n"<<g;
-    
+    cout << setfill('*') << setw(10) << "Hello" << endl;//用*補滿前綴
+    double p = 22.0/7.0;
+    cout<<"p="<<setprecision(4)<<p<<endl; //四捨五入至第4位
+    cout  <<  fixed <<  setprecision(n) << endl; //小數點後的n位，沒有四捨五入
     return 0;
 
 }
