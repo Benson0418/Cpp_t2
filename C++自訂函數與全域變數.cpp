7c5394170eb766bde471dae107fdd481bb41a3de
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2023-11-11 17:08:57 +0800

    Update and rename C++自訂函數 隨筆.cpp to C++自訂函數與全域變數.cpp

---
+++ C++自訂函數與全域變數.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int global_variable;
+
+void hello_world(){printf("Hello, World!\n");}
+
+void hello_cpp(){
+    hello_world();
+    printf("Hello, C++!\n");
+}
+void uwu(int x){
+    printf("%d\n", x);
+    return;
+    cout<<"這行代碼不會被執行";
+}
+
+//傳入的是數組首項的指標及數組本身的大小
+int add2(int x[], int len){
+    int res=0;
+    for (int i=0;i<len;++i)res+=x[i];
+    return res;
+}
+
+void g(){
+    global_variable=100;
+    return;
+}
+
+//c++中的遞迴寫法
+int f(int n){
+    if (n==1||n==0)return 1;
+    return f(n-1)+f(n-2);
+}
+
+int main(){
+    hello_world();
+    hello_cpp();
+    int a;
+    scanf("%d", &a);
+    uwu(a);
+
+    int t[]={1,2,3,4,5,6,7,8,9};
+    int len=sizeof(t)/sizeof(t[0]);
+    printf("%d\n", add2(t, len));
+
+
+    //全域變數的使用
+    cin>>global_variable;
+    cout<<global_variable<<'\n';
+    g();
+    cout<<global_variable<<'\n';
+
+    int c;
+    scanf("%d", &c);
+    cout<<f(c);
+
+}
