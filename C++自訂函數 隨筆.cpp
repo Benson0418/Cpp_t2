e8518d12ea90865498bdfae91d3c7dca150ae73c
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2023-11-11 15:53:28 +0800

    Create C++自訂函數 隨筆.cpp

---
+++ C++自訂函數 隨筆.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void hello_world(){printf("Hello, World!\n");}
+
+void hello_cpp(){
+    hello_world();
+    printf("Hello, C++!\n");
+}
+
+int main(){
+    hello_world();
+    hello_cpp();
+}
